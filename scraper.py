e29bc5fd877abbf10155039942e564ae467fc799
sam soleymani <amssirani@gmail.com>
2023-11-23 15:49:16 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
+#import the modules
 from bs4 import BeautifulSoup
 import requests
+#get the page from google link
 html_text = requests.get(input("link: ")).text
+#create the BS object by the lxml parser
 content = BeautifulSoup(html_text, 'lxml')
+#find headlines
 tags = content.find_all('h3')
+#print them
 for tag in tags:
     print(tag.text)
